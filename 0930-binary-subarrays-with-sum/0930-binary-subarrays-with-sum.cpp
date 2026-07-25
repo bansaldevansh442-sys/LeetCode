669fcd7fc05fc2ee7e98a31db79b8a59dd8bc262
bansaldevansh442-sys <bansaldevansh442@gmail.com>
2026-07-26 02:43:35 +0530

    Time: 0 ms (100%), Space: 32.7 MB (39.75%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+int lessthanequalgoal(vector<int>& nums, int goal){
+        if(goal<0)return 0;
+        int l=0;
+        int r=0;
+        int sum=0;
+        int cnt=0;
+        while(r<nums.size()){
+           if(sum<=goal){
+            sum+=nums[r];
+           } 
+          while(sum>goal){
+            sum=sum-nums[l];
+            l++;
+          }
+          cnt=cnt+(r-l+1);
+          r++;
+        }
+        return cnt;
+}
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return lessthanequalgoal(nums,goal)-lessthanequalgoal(nums,goal-1);
+    }
+};
