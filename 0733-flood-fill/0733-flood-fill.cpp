6bbc247c787466a09d09ac4c6e0cd2e04116b8c9
bansaldevansh442-sys <bansaldevansh442@gmail.com>
2026-08-06 02:33:56 +0530

    Time: 3 ms (12.62%), Space: 18.4 MB (30.97%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-void dfs(vector<vector<int>>&ans,vector<vector<int>>& image,int inicolour, int row, int col, int color){
+void bfs(vector<vector<int>>&ans,vector<vector<int>>& image,int inicolour, int row, int col, int color){
     ans[row][col]=color;
      int n=image.size();
      int m=image[0].size();
+     queue<pair<int,int>>q;
+     q.push({row,col});
+     while(!q.empty()){
+        int row=q.front().first;
+        int col=q.front().second;
+        q.pop();
      vector<int>delcol={-1,0,1,0};
      vector<int>delrow={0,1,0,-1};
      for(int i=0;i<4;i++){
         int nrow=delrow[i]+row;
         int ncol=delcol[i]+col;
-        if(nrow>=0 &&nrow< n &&ncol>=0 &&ncol<m && image[nrow][ncol]==inicolour &&ans[nrow][ncol]!=color )
-        dfs(ans,image,inicolour,nrow,ncol,color);
+        if(nrow>=0 &&nrow< n &&ncol>=0 &&ncol<m && image[nrow][ncol]==inicolour &&ans[nrow][ncol]!=color ){
+            ans[nrow][ncol]=color;
+            q.push({nrow,ncol});
+        }
+     }
      }
 
 }
@@ -19,7 +28,7 @@ void dfs(vector<vector<int>>&ans,vector<vector<int>>& image,int inicolour, int r
         int n=image.size();
         int m=image[0].size();
         vector<vector<int>>ans=image;
-        dfs(ans,image,inicolour,sr,sc,color);
+        bfs(ans,image,inicolour,sr,sc,color);
         return ans;
     }
 };
