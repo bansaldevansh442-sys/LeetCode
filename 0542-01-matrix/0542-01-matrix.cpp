852f4361d3eb1e567059d5897ddfd0283f44ab0c
bansaldevansh442-sys <bansaldevansh442@gmail.com>
2026-08-11 17:15:42 +0530

    Time: 32 ms (15.77%), Space: 39 MB (20.04%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -10,7 +10,7 @@ public:
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
                     q.push({{i,j},0});
-                    vis[i][j]=1;
+                    // vis[i][j]=1;
                 }
                 else
                  vis[i][j]=0;
